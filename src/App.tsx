626996cea602c92bdefaaabc561d7f2d1e2f51dc
VeroLom <ver@0xff.su>
2023-10-07 13:22:33 +0200

    Added simple routing and Home/Users pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import UserList from "./components/UserList";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import UserListPage from "./pages/Users/UserListPage";
+import HomePage from "./pages/Users/HomePage";
 
 function App() {
-  return (
-    <div className="app">
-        <UserList />
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <div>
+                    <Link to={"/"}>Home</Link>
+                    <Link to={"/users"}>Users</Link>
+                </div>
+                <div>
+                    <Routes>
+                        <Route path={"/"} element={<HomePage />} />
+                        <Route path={"/users"} element={<UserListPage />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
